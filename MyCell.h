4b2c1388f66a4c42c9e45d7a8c84ed35679b5396
Prashant Gaikwad <prashant@scriptlanes.com>
2016-05-04 17:39:18 +0530

    first commit

---
+++ MyCell.h
@@ -0,0 +1,14 @@
+//
+//  MyCell.h
+//  AroundMe
+//
+//  Created by Prashant Gaikwad
+
+#import <UIKit/UIKit.h>
+
+@interface MyCell : UITableViewCell
+@property (strong, nonatomic) IBOutlet UIImageView *placeImageView;
+
+@property (strong, nonatomic) IBOutlet UILabel *placeNameLabel;
+
+@end
---
+++ MyCell.m
@@ -0,0 +1,22 @@
+//
+//  MyCell.m
+//  AroundMe
+//
+//  Created by Prashant Gaikwad
+
+#import "MyCell.h"
+
+@implementation MyCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
